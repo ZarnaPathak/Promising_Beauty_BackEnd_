b4b0624a5645865215bc64707928443827e224ef
pm1019 <pm10920407@gmail.com>
2023-01-12 23:49:06 +0530

    Pinky/App/Dashboard created + linked!

--- promising_beauty/Dashboard/views.py
+++ promising_beauty/Dashboard/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def Dashboard(request):
     return render(request,'customer_dashboard.html')
+
+def Dashboard_cart(request):
+    return render(request,'customer_cart.html')
+
+
