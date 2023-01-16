fc0eee237e04f8350c66a36c9d570f202fb422a8
pm1019 <pm10920407@gmail.com>
2023-01-16 15:00:55 +0530

    Pinky/App/Customization  hoop and product page linked!

--- promising_beauty/customization/urls.py
+++ promising_beauty/customization/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,URLPattern
 from . import views
 
 urlpatterns = [
-    path('custom_page',views.customization,name="customization")
+    path('custom_page',views.customization,name="customization"),
+    path('custom_hoop',views.customization_hoop,name="customization_hoop"),
+    path('custom_dress',views.customization_dress,name="customization_dress")
 ]
--- promising_beauty/customization/views.py
+++ promising_beauty/customization/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def customization(request):
     return render(request,'customizationhome.html')
+
+def customization_hoop(request):
+    return render(request,'hoop.html')  
+
+def customization_dress(request):
+    return render(request,'product_customization.html')
+
