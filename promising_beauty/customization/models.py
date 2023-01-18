a9349888404eafaa341f1ef8efdc43fd60096dd7
pm1019 <pm10920407@gmail.com>
2023-01-18 17:40:42 +0530

    Pinky/Model/Customization created + linked

--- promising_beauty/customization/models.py
+++ promising_beauty/customization/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class customizationhome(models.Model):
+    custm_id=models.IntegerField(primary_key=True)
+    custm_type=models.CharField(max_length=10)
+
+class hoop(models.Model):
+    custm_id=models.ForeignKey('customizationhome',default=0,on_delete=models.SET_DEFAULT)
+    hoop_id=models.IntegerField(primary_key=True)
+    hoop_layout=models.IntegerField()
+    hoop_name1=models.CharField(max_length=100)
+    hoop_name2=models.CharField(max_length=100)
+    hoop_date=models.DateField()
+    hoop_msg=models.TextField(max_length=30)
+
