a78662a5a634bee6ca29737fb7bf094d84753526
Zarna Pathak <pathakzarna0128@gmail.com>
2023-01-18 17:44:47 +0530

    Zarna/Model/Product created + Linked

--- promising_beauty/Products/admin.py
+++ promising_beauty/Products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from Products.models import Cat,SubCat,P_Details
+from django.contrib.admin.sites import site
 # Register your models here.
+class CatAdmin(admin.ModelAdmin):
+    list_display=('cat_id','cat_name')
+
+class SubCatAdmin(admin.ModelAdmin):
+    list_display=('subcat_id','cat_id','subcat_name')
+
+class PDetailsAdmin(admin.ModelAdmin):
+    list_display=('p_id','subcat_id','p_name','p_price','p_qty','p_type','p_fabric','p_size','p_img','p_desc','p_color')
+
+
+admin.site.register(Cat,CatAdmin)
+admin.site.register(SubCat,SubCatAdmin)
+admin.site.register(P_Details,PDetailsAdmin)
